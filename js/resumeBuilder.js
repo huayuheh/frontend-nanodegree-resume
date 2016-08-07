27129547b98a9711db2ce65bf146151e76100238
huayuheh <anubiss13477@gmail.com>
2016-08-07 15:07:13 -0700

    show funThought on page
    show funThought on page, not use console.log

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Huayu Heh");
+
  var awesomeThoughts = "I am Huayu and I am AWESOME!";
  console.log(awesomeThoughts);
  var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
  console.log(funThoughts);
+  $("#main").append(funThoughts);
