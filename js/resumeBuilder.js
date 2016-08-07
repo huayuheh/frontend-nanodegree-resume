80ef1d3070af6de13b676f5f3d8677923967d005
huayuheh <anubiss13477@gmail.com>
2016-08-07 15:01:20 -0700

    append function
    use append function to add my name on the index page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ $("#main").append("Huayu Heh");
+ var awesomeThoughts = "I am Huayu and I am AWESOME!";
+ console.log(awesomeThoughts);
