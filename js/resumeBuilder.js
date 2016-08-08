91a429a091a44f6fff18928cc1937af8968d5dc7
huayuheh <anubiss13477@gmail.com>
2016-08-07 21:12:11 -0700

    add work and education
    add work array and education array in the file, and show info on main
    page.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -13,3 +13,21 @@ var role = "Front-End Developer"
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%",role);
 $("#header").prepend(formattedName, formattedRole);
+
+var skill = ["UI design","UX design","JS","drawing"];
+console.log(skill.length);
+
+
+var work ={ };
+	work.position = "UI Designer";
+	work.employer = "JYreading";
+	work.year = 0.3;
+
+var education = {};
+	education["name"] = "Acadamy of Art University";
+	education["year"] = "2015-2017";
+	education["city"] = "San Francisco, CA, US";
+
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
